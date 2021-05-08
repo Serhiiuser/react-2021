380c5854277dcd3be4e0fd29922bef1813225a10
SK <serhiik91@gmail.com>
2021-05-08 12:50:40 +0300

    homeWork1

---
+++ src/components/Cars.js
@@ -0,0 +1,10 @@
+export default function Cars(props) {
+     let {model,description,power,volume} = props;
+     return (
+    <div>
+        <h2>{model}</h2>
+        <p>{description}</p>
+        <p>{power} {volume}</p>
+    </div>
+);
+}
