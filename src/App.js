f18b620c7abf15f2ab52c9d523fb35d72ee5003a
SK <serhiik91@gmail.com>
2021-05-12 16:40:03 +0300

    homeWork4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {
+ BrowserRouter  as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Home from "./components/home/Home";
+import Users from "./components/users/Users";
+import Posts from "./components/posts/PostsFile"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+        <Link to{`/`}>home page</Link>
+          <br/>
+          <Link to{`/users`}>users page</Link>
+          <br/>
+          {/*<Link to{`/posts`}>posts page</Link>*/}
+          <br/>
+          {/*<Link to{`/coments`}>comments page</Link>*/}
+          <hr/>
+          <Switch>
+     <Route exact path={`/`}>
+       <Home/>
+       </Route>
+            <Route path={`/users`} component={Users}/>
+              {/*<Route path={`/posts`} component={Posts}/>*/}
+              {/*<Route path={`/comments`} component={Сomments}/>*/}
+          </Switch>
+
+      </div>
+
+      </Router>
+
   );
 }
 
